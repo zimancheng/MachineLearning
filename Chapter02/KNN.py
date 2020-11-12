455c2547babb28f8dd757e3ac90e3d03f29a75c8
zimancheng <46982457+zimancheng@users.noreply.github.com>
2020-11-12 21:31:25 +0800

    Add files via upload
    Finished Ch02

--- Chapter02/KNN.py
+++ Chapter02/KNN.py
@@ -39,8 +39,8 @@ def file2matrix(filename):
     output: a matrix of training examples and a list of labels for each person
     '''
     #readline and convert lines to a array
-    file = open(filename)
-    lines = file.readlines()
+    fr = open(filename)
+    lines = fr.readlines()
     lineNum = len(lines)
 
     #for each line, put the first 3 cols as features and put them in a featere matrix
@@ -54,7 +54,7 @@ def file2matrix(filename):
         lineArr = line.split('\t')
         ftrMat[index, :] = lineArr[0:3]
         if (lineArr[-1].isdigit()):
-            lblArr.append(lineArr[-1])
+            lblArr.append(int(lineArr[-1]))
         else:
             lblArr.append(love_dict.get(lineArr[-1]))
         index += 1
@@ -104,10 +104,73 @@ def datingClassTest():
 def classifyPerson():
     '''
     input: take input from user
-    output: the class the person belongs to
-            
+    output: the class the person belongs to      
     note that: love_dict = {'largeDoses':3, 'smallDoses':2, 'didntLike':1}
     '''
+    lblList = ['not at all', 'in small doses', 'in large doses']
+    icecream = float(input("Liters of ice cream consumed per year?"))
+    flyMiles = float(input("Frequent flier miles earned per year?"))
+    vidPct = float(input("Percentage of time spent playing video games?"))
+    inArr = np.array([flyMiles, vidPct, icecream]) #create the array as inputX
+
+    #create the training data set
+    trainMat, trainLbl = file2matrix("datingTestSet2.txt")
+    normMat, ranges, minVals = autoNorm(trainMat)
+
+    #classify the person
+    label = classify0((inArr - minVals)/ranges, normMat, trainLbl, 3)
+    print(f"You will probably like this person: {lblList[label - 1]}")
+
+def img2Vec(filename):
+    '''
+    input: the filedir of each img txt file
+    output: the 1*1024 numpy matrix, make it matrix for future classificationn
+    '''
+    arr = np.zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        line = fr.readline()
+        for j in range(32):
+            arr[0, i*32 + j] = line[j]
+    return arr
+
+def handwritingClassTest():
+    '''
+    returns the error rate of testDigits image classification
+    '''
+    # set up the training data set
+    # create a m*1024 matrix to contain all training image vectors
+    # create label list for the true numbers of each image
+    train_img_list = listdir("trainingDigits") #['7_10.txt', '7_11.txt', ...]
+    train_m = len(train_img_list)
+    trainMat = np.zeros((train_m, 1024))
+    train_lbl_list = []
+
+    for i in range(train_m):
+        img_file_name = train_img_list[i] #'7_10.txt'
+        img_lbl = int(img_file_name.split('_')[0])
+        train_lbl_list.append(img_lbl)
+        trainMat[i, :] = img2Vec("trainingDigits/" + img_file_name)
+    
+    # classify the test data set 
+    test_img_list = listdir("testDigits")
+    test_m = len(test_img_list)
+    test_err_cnt = 0
+
+    for i in range(test_m):
+        img_file_name = test_img_list[i]
+        img_lbl = int(img_file_name.split('_')[0])
+        test_arr = img2Vec("testDigits/" + img_file_name)
+        pred_lbl = classify0(test_arr, trainMat, train_lbl_list, 3)
+        print(f"The classifier came back with: {pred_lbl}, the real answer is: {img_lbl}")
+        if pred_lbl != img_lbl:
+            test_err_cnt += 1
+    
+    print(f"\nThe total number of errors is: {test_err_cnt}")
+    print(f"\nThe total error rate is: {test_err_cnt/test_m}")
+
+
+
 
 # print("test for reloading module")
 
