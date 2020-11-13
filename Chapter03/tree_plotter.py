2c7c1d25ceba9d6f42063c14b849debd8bcbe1a2
zimancheng <46982457+zimancheng@users.noreply.github.com>
2020-11-13 23:32:44 +0800

    Add files via upload

---
+++ Chapter03/tree_plotter.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt 
+
+decision_node = dict(boxstyle = "sawtooth", fc = "0.8")
+leaf_node = dict(boxstyle = "round4", fc = "0.8")
+arrow_args = dict(arrowstyle = "<-")
+
